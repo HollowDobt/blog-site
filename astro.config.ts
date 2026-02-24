4f6a890e17539b977390c149163d702d6443b2ab
HollowDobt <2901448780@qq.com>
2026-02-24 10:31:57 +0800

    feat: bootstrap static thought-lite and self-hosted comment integration

---
+++ astro.config.ts
@@ -0,0 +1,110 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import yaml from "@rollup/plugin-yaml";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
+import swup from "@swup/astro";
+
+import GFM from "remark-gfm";
+import ins from "remark-ins";
+import mark from "remark-flexible-markers";
+import spoiler from "@tuyuritio/remark-spoiler";
+import CJK from "remark-cjk-friendly";
+import CJKStrikethrough from "remark-cjk-friendly-gfm-strikethrough";
+import ruby from "@tuyuritio/remark-ruby";
+import attr from "@tuyuritio/remark-attribute";
+import math from "remark-math";
+import gemoji from "remark-gemoji";
+import footnote from "remark-footnotes-extra";
+import abbr from "@tuyuritio/remark-abbreviation";
+import { remarkExtendedTable as table, extendedTableHandlers as tableHandler } from "remark-extended-table";
+import alerts from "@tuyuritio/remark-github-alert";
+import { rehypeHeadingIds as ids } from "@astrojs/markdown-remark";
+import anchor from "rehype-autolink-headings";
+import links from "rehype-external-links";
+import katex from "rehype-katex";
+import figure from "@tuyuritio/rehype-image-figure";
+import wrapper from "@tuyuritio/rehype-table-wrapper";
+import sectionize from "@hbsnow/rehype-sectionize";
+import copy from "@tuyuritio/shiki-code-copy";
+
+import reading from "./src/utils/reading";
+
+import siteConfig from "./site.config";
+
+// https://astro.build/config
+export default defineConfig({
+	output: "static",
+	site: "https://blog.hollowdobt.com",
+	trailingSlash: "never",
+	i18n: {
+		...siteConfig.i18n,
+		routing: {
+			redirectToDefaultLocale: false,
+			prefixDefaultLocale: false
+		}
+	},
+	markdown: {
+		remarkPlugins: [
+			[GFM, { singleTilde: false }],
+			ins,
+			mark,
+			spoiler,
+			CJK,
+			[CJKStrikethrough, { singleTilde: false }],
+			ruby,
+			attr,
+			math,
+			gemoji,
+			footnote,
+			abbr,
+			[table, { colspanWithEmpty: true }],
+			[alerts, { typeFormat: "capitalize" }],
+			reading
+		],
+		remarkRehype: {
+			footnoteLabel: null,
+			footnoteLabelTagName: "p",
+			footnoteLabelProperties: {
+				className: ["hidden"]
+			},
+			handlers: {
+				...tableHandler
+			}
+		},
+		rehypePlugins: [
+			ids,
+			[anchor, { behavior: "wrap" }],
+			[links, { target: "_blank", rel: ["nofollow", "noopener", "noreferrer"] }],
+			katex,
+			figure,
+			wrapper,
+			sectionize
+		],
+		smartypants: false,
+		shikiConfig: {
+			themes: {
+				light: "github-light",
+				dark: "dark-plus"
+			},
+			transformers: [copy({ duration: 1500 })]
+		}
+	},
+	vite: {
+		// @ts-expect-error
+		plugins: [yaml(), tailwindcss()]
+	},
+	integrations: [
+		svelte(),
+		mdx(),
+		sitemap(),
+		swup({
+			globalInstance: true,
+			preload: false,
+			smoothScrolling: false,
+			progress: true
+		})
+	]
+});
