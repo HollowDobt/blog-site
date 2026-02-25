fb589bfe6a35694dbd1c82e970001f3a4cab9b1c
HollowDobt <2901448780@qq.com>
2026-02-25 15:04:20 +0800

    fix(config): restore named exports required by app

--- site.config.ts
+++ site.config.ts
@@ -41,4 +41,17 @@ const config = siteConfig({
 
 const monolocale = Number(config.i18n.locales.length) === 1;
 
+const turnstile = env.CLOUDFLARE_TURNSTILE_SECRET_KEY ? env.CLOUDFLARE_TURNSTILE_SITE_KEY : undefined;
+
+const push = env.VAPID_PRIVATE_KEY ? env.VAPID_PUBLIC_KEY : undefined;
+
+const email = Boolean(env.EMAIL_FROM);
+
+const oauth = providers([
+	{ name: "Google", logo: "simple-icons--google", clientID: env.GOOGLE_CLIENT_ID, clientSecret: env.GOOGLE_CLIENT_SECRET },
+	{ name: "X", logo: "simple-icons--x", clientID: env.TWITTER_CLIENT_ID, clientSecret: env.TWITTER_CLIENT_SECRET }
+]);
+
+export { turnstile, oauth, monolocale, push, email };
+
 export default config;
