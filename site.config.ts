3cbb323439dec39744b925904c879821506491ce
HollowDobt <2901448780@qq.com>
2026-02-25 14:59:49 +0800

    Fixed

--- site.config.ts
+++ site.config.ts
@@ -3,14 +3,14 @@ import siteConfig, { providers } from "./src/utils/config";
 const env = import.meta.env ?? {};
 
 const config = siteConfig({
-	title: "Hollow Dobt",
+	title: "Hollow's Corner",
 	prologue: "Before the first move, I slow my pulse.\nThis is Hollow's corner.\nRules are dismantled here, then rewritten as stories.",
 	author: {
 		name: "Hollow Dobt",
 		email: "i@hollowdobt.com",
 		link: "https://blog.hollowdobt.com"
 	},
-	description: "Hollow Dobt blog",
+	description: "帆楼的个人博客",
 	copyright: {
 		type: "CC BY-NC-ND 4.0",
 		year: "2026"
@@ -40,18 +40,5 @@ const config = siteConfig({
 });
 
 const monolocale = Number(config.i18n.locales.length) === 1;
-
-const turnstile = env.CLOUDFLARE_TURNSTILE_SECRET_KEY ? env.CLOUDFLARE_TURNSTILE_SITE_KEY : undefined;
-
-const push = env.VAPID_PRIVATE_KEY ? env.VAPID_PUBLIC_KEY : undefined;
-
-const email = Boolean(env.EMAIL_FROM);
-
-const oauth = providers([
-	{ name: "Google", logo: "simple-icons--google", clientID: env.GOOGLE_CLIENT_ID, clientSecret: env.GOOGLE_CLIENT_SECRET },
-	{ name: "X", logo: "simple-icons--x", clientID: env.TWITTER_CLIENT_ID, clientSecret: env.TWITTER_CLIENT_SECRET }
-]);
-
-export { turnstile, oauth, monolocale, push, email };
 
 export default config;
